4494fabed763b0b610558426fbc35c43773f79f9
Raffaele Sollecito <raffasolaries@gmail.com>
2021-06-15 17:45:35 +0200

    database update

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -34,8 +34,8 @@ resource "aws_rds_cluster" "aurora" {
  master_password         = var.db_credentials.master_password
  backup_retention_period = var.db_backup_retention_period
  preferred_backup_window = var.db_backup_window
- vpc_security_group_ids  = [aws_db_security_group.rds_sg.id]
- db_subnet_group_name    = aws_db_subnet_group.rds_private_subnet.name
+ vpc_security_group_ids  = [aws_db_security_group.rds_sg[0].id]
+ db_subnet_group_name    = aws_db_subnet_group.rds_private_subnet[0].name
  deletion_protection     = true
  scaling_configuration {
   max_capacity             = var.db_type == "mysql" ? 256 : 384
@@ -60,7 +60,7 @@ resource "aws_secretsmanager_secret" "rds" {
 }
 resource "aws_secretsmanager_secret_version" "rds" {
  count                   = var.relational_db == true ? 1 : 0
- secret_id               = aws_secretsmanager_secret.rds.id
+ secret_id               = aws_secretsmanager_secret.rds[0].id
  secret_string           = jsonencode(var.db_credentials)
  tags = {
   Name = "${var.environment}-${var.app_name}-aurora-secrets-version"
@@ -77,6 +77,14 @@ resource "aws_dynamodb_table" "db" {
  stream_enabled           = false
  point_in_time_recovery {
   enabled = true
+ }
+ attribute {
+  name = var.db_hash_key
+  type = "S"
+ }
+ attribute {
+  name = var.db_range_key
+  type = "S"
  }
  tags = {
   Name = "${var.environment}-${var.app_name}-dynamodb-table"
